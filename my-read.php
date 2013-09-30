82b8338c520467700aeaf3a4ce62c89a5a183c8c
william <iamhandian@gmail.com>
2013-09-30 10:34:33 +0800

    add homepage , search-list page and my-read page

---
+++ my-read.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My-Read</title>
+	<link rel="stylesheet" href="css/style.css">
+	<meta name="viewport" content="initial-scale=1">
+</head>
+<body>
+	<header>
+		我读过的书
+	</header>
+	<div class="main">
+		<ul class="my-read-list">
+			<li>
+				<a href="">
+					<img class="bookcover" src="img/book-steve.jpg" alt="">
+					<div class="bookinfo">
+						<span class="bookname">史蒂夫·乔布斯传</span>
+						<p class="bookauthor">沃尔特·艾萨克森</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="bookcover" src="img/book-steve.jpg" alt="">
+					<div class="bookinfo">
+						<span class="bookname">史蒂夫·乔布斯传</span>
+						<p class="bookauthor">沃尔特·艾萨克森</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="bookcover" src="img/book-steve.jpg" alt="">
+					<div class="bookinfo">
+						<span class="bookname">史蒂夫·乔布斯传</span>
+						<p class="bookauthor">沃尔特·艾萨克森</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="bookcover" src="img/book-steve.jpg" alt="">
+					<div class="bookinfo">
+						<span class="bookname">史蒂夫·乔布斯传</span>
+						<p class="bookauthor">沃尔特·艾萨克森</p>
+					</div>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<footer>
+		<ul class="footer-nav">
+			<li><a href="">想读</a></li>
+			<li><a href="">在读</a></li>
+			<li><a href="">已读</a></li>
+		</ul>
+	</footer>
+</body>
+</html>
