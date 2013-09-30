dd48b8f8183719d2a83bf28c8427a61f990b06e2
william <iamhandian@gmail.com>
2013-09-30 15:12:30 +0800

    add book page

--- book.php
+++ book.php
@@ -19,10 +19,35 @@
 			<div class="bookinfo">
 				<span class="bookname">史蒂夫·乔布斯传</span>
 				<p class="bookauthor">沃尔特·艾萨克森</p>
+				<div class="read-btns">
+					<a class="btn read-btn toread" href="">想读</a>
+					<a class="btn read-btn reading" href="">在读</a>
+					<a class="btn read-btn read" href="">已读</a>
+				</div>
 			</div>
 		</div>
-		
-		
+		<div class="book-des">
+			<div class="title">内容简介</div>
+			<p class="describe">这本乔布斯唯一授权的官方传记，在2011年上半年由美国出版商西蒙舒斯特对外发布出版消息以来，备受全球媒体和业界瞩目，这本书的全球出版日期最终确定为2011年11月21日，简体中文版也将同步上市。</p>
+			<p class="describe">两年多的时间，与乔布斯40多次的面对面倾谈，以及与乔布斯一百多个家庭成员、 朋友、竞争对手、同事的不受限的采访，造就了这本独家传记。</p>
+			<p class="describe">尽管乔布斯给予本书的采访和创作全面的配合，但他对内容从不干涉，也不要求出版前阅读全文的权利。对于任何资源和关联的人，他都不设限，甚至鼓励他所熟知的人袒露出自己的心声。</p>
+			<p class="describe">“我已经做了很多并不值得自豪的事情，比如23岁时就让我的女友怀了孕，以及我对这件事的处理方式”，他说， “对我而言，没有什么不可以对外袒露的。”</p>
+		</div>
+		<div class="book-content">
+			<div class="title">目录</div>
+			<ul class="contents">
+				<li>主要人物</li>
+				<li>前言 本书是如何诞生的</li>
+				<li>第一章 童年</li>
+				<li>第二章 奇特的一对</li>
+				<li>第三章 出离</li>
+				<li>第四章 Apple I</li>
+				<li>第五章 Apple II</li>
+				<li>第六章 克里斯安和丽萨</li>
+				<li>第七章 施乐和丽萨</li>
+				<li>第八章 上市</li>
+			</ul>
+		</div>
 	</div>
 	<footer>
 		<ul class="footer-nav">
